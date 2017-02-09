0e76031e57ecb0b6f0bc9fed1fa8f1de05455046
dillerm <dillerm@oregonstate.edu>
2017-02-09 09:03:42 -0800

    converted to php and added gitignore

--- templates/header.php
+++ templates/header.php
@@ -46,32 +46,3 @@
 
 	<!-- Page Content -->
 	<div class="container">
-
-	<h2>Electronic Design</h2>
-
-	<p>Metratek provides expertise in electronic instrument design, with expertise in the following areas:</p>
-	<ul>
-		<li>Wide bandwidth analog (DC to > 1GHz), time and frequency domain</li>
-		<li>Precision DC analog</li>
-		<li>Signal generation and acquisition</li>
-		<li>Linear and switching power supplies</li>
-		<li>System level instrument architecture</li>
-	</ul>
-	<p>With over 30 years in the test and measurement industry, we have the experience to help you solve your difficult measurement problems.</p>
-
-	 
-	<h2>Software Development</h2>
-
-	<p>Metratek also develops custom software applications for engineering and production test.  We provide assistance all the way from specifying the test through implementing the application.   Areas of expertise include:</p>
-	<ul>
-		<li>Visual Basic</li>
-		<li>C/C++</li>
-		<li>Windows</li>
-		<li>Instrument interfacing - Oscilloscopes, Meters, Spectrum analyzers, Network analyzers</li>
-	</ul>
- 
-	</div>
-	<!-- /.container -->
-</body>
-
-</html>
